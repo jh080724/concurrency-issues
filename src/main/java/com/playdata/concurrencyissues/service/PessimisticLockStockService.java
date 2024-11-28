54514598da0892f8f2fb031ffb0d41da728b4773
jh080724 <tsj010813@gmail.com>
2024-11-28 16:46:22 +0900

    PessimisticLock을 사용한 DB Lock - 비관적 Lock 적용

---
+++ src/main/java/com/playdata/concurrencyissues/service/PessimisticLockStockService.java
@@ -0,0 +1,29 @@
+package com.playdata.concurrencyissues.service;
+
+import com.playdata.concurrencyissues.entity.Stock;
+import com.playdata.concurrencyissues.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+@Transactional
+public class PessimisticLockStockService {
+    private final StockRepository stockRepository;
+
+    // 주문이 들어오면 재고를 감소시키는 메소드
+    public void decrease(Long id, Long quantity){
+        // Stock 조회
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+
+        // 재고 감소 진행
+        stock.decrease(quantity);
+
+        // 재고 수정된 값을 저장
+        stockRepository.save(stock);
+
+    }
+}
